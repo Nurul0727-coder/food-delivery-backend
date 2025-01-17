8d87bd0b5e910f223fed5b420ab02eb3a993a537
24LP6048 24LP6048 <24LP6048@G302-CC0089.local>
2025-01-17 18:08:32 +0800

    added some changes

--- index.ts
+++ index.ts
@@ -1,92 +1,78 @@
-import { configDotenv } from 'dotenv';
-import express, {Request, Response} from 'express';
-const mongoose = require ('mongoose')
-const cors = require ('cors')
+import express, { Request, Response } from "express";
+import { config, configDotenv } from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
 
-
-const PORT = 8005;
-const app = express ();
+const PORT = 8000;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 configDotenv();
 
-const connectMongoDB= async()=> {
-    const MONGODB_URI = process.env.MONGODB_URI;
+const connectMongoDB = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (MONGODB_URI) {
     await mongoose.connect(MONGODB_URI);
+  }
 };
 
- const FOOD_CATEGORY_SCHEMA=new mongoose.Schema({
-    categoryName:String,
- },
- {timestamps:true}
+const FOOD_CATEGORY_SCHEMA = new mongoose.Schema(
+  {
+    categoryName: String,
+  },
+  { timestamps: true }
 );
 
-connectMongoDB();   
+connectMongoDB();
 
-app.get('/', async (req:Request, res:Response)=>{
-    const database=mongoose.db('Food-delivery');
-    const collection= database.collection('food-category');
+// FoodCategoryModel.find
 
-    //Fetch all documdents from the collections
-    const results = await collection.find().toArray();
-    console.log(results);
+const FoodCategoryModel = mongoose.model(
+  "FoodCategory",
+  FOOD_CATEGORY_SCHEMA,
+  "food-category"
+);
 
-    const newItem = await collection.insertOne({
-        categoryName: 'Fast Foods'
-    })
-    const newResults = await collection.find().toArray();
-    console.log(newResults);
-
-    res.send('Food');
+app.get("/food-category/", async (req: Request, res: Response) => {
+  const data = await FoodCategoryModel.find();
+  res.json(data);
 });
 
- const FoodCategoryModel=mongoose.model(
-    'FoodCategory',
-     FOOD_CATEGORY_SCHEMA,
-     'food-category')
-
-app.get('/food-category/',async (req: Request, res: Response)=>{
-     const data = await FoodCategoryModel.find();
-     res.json(data);
+app.get(".food-category/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const item = await FoodCategoryModel.findById;
+  res.json(item);
 });
 
-app.get('.food-category/:id', async (req:Request,res:Response)=>{
-    const id=req.params.id;
-    const item = await FoodCategoryModel.findbyId
-    res.json(item);
+app.delete("/food-category/:id", async (req: Request, res: Response) => {
+  const deletedItem = await FoodCategoryModel.findByIdAndDelete(req.params.id);
+  res.json(deletedItem);
 });
 
-
-app.delete('/food-category/:id', async (req:Request, res:Response)=>{
-    const deletedItem = await FoodCategoryModel.findByIdAndDelete(req.params.id)
-    res.json(deletedItem);
+app.post("/food-category/", async (req: Request, res: Response) => {
+  const newItem = await FoodCategoryModel.create({
+    categoryName: req.body.categoryName,
+  });
+  res.json({
+    message: "New Food Category created successfully.",
+    newItem,
+  });
 });
 
-app.post('/food-category/', async (req:Request, res:Response)=>{
-    const newItem= await FoodCategoryModel.create({
-        categoryName: req.body.categoryName,
-    });
-    res.json({
-        message: 'New Food Category created successfully.',
-        newItem,
-    })
-});
-
-app.put('/food-category/:id', async (req:Request, res:Response)=>{
-    const updatedItem= await FoodCategoryModel.findByIdAndUpdate(
-        req.params.id, 
-        {
-        categoryName: req.body.categoryName,
+app.put("/food-category/:id", async (req: Request, res: Response) => {
+  const updatedItem = await FoodCategoryModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      categoryName: req.body.categoryName,
     },
-    {new: true }
-);
+    { new: true }
+  );
 
-    res.json(updatedItem);
+  res.json(updatedItem);
 });
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
